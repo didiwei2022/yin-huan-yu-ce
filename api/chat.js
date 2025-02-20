adb6e769a0bd22fe33d766d6e6b50a3e202593f6
didiwei2022 <1164144970@qq.com>
2025-02-20 12:26:48 +0800

    添加 CORS 支持

--- api/chat.js
+++ api/chat.js
@@ -1,4 +1,7 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
   console.log('[API] 请求开始:', {
     method: req.method,
     url: req.url,
